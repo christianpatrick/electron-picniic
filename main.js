7a58bd7a326f137c19dc3d1773057c31f04ba159
Christian Helvin <christian@ignitehere.com>
2018-03-01 15:47:52 -0600

    fixed the splash look

--- main.js
+++ main.js
@@ -248,5 +248,5 @@ app.on('window-all-closed', function () {
 app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-    mainWindow.show();
+    mainWindow.show()
 })
